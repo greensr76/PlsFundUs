7ac839a9aea94bcbadd8c19c506368d726cbe964
tduvall <47044650+tduvall@users.noreply.github.com>
2019-04-17 12:26:01 -0500

    Added 'Register' to navbar

--- javascript/functions.js
+++ javascript/functions.js
@@ -3,11 +3,24 @@ window.onload = function () {
 }
 
 var generateNav = function () {
-    var container = document.createElement("div");
-    container.setAttribute("class", "navbar");
+    var container = document.createElement("section");
+    container.setAttribute("class", "nav");
+
+    var navLeft = document.createElement("section");
     var brand = document.createElement("a");
-    brand.setAttribute("href", "/index.html");
+    brand.setAttribute("href", "index.html");
     brand.appendChild(document.createTextNode("Brand image here"));
-    container.appendChild(brand);
-    document.getElementById("nav").appendChild(container);
+    navLeft.appendChild(brand);
+
+    var navRight = document.createElement("section");
+    var register = document.createElement("a");
+    register.setAttribute("href", "#");
+    navRight.setAttribute("class", "nav-right");
+    register.appendChild(document.createTextNode("Register"));
+    navRight.appendChild(register);
+
+    container.appendChild(navLeft);
+    container.appendChild(navRight);
+    
+    document.body.insertBefore(container, document.body.firstChild);
 }
