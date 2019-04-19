667320bb72f88e470734f6da7a92e4b0736756c6
Stephen Green <stephenrgreen76@gmail.com>
2019-04-19 13:22:18 -0500

    Added Registration option to login page
    Updated TODO list on index

--- php/login.php
+++ php/login.php
@@ -10,23 +10,44 @@
     $username = $_POST["username"];
     $password = $_POST["password"];
 
-    $sql = "SELECT ID FROM users WHERE NAME = '$username' AND PASSWORD = '$password'";
-    $result = $conn->query($sql);
-
-    if ($result->num_rows > 0) {
-        $row = $result->fetch_assoc();
-
-        echo "Login Successful";
-        $_SESSOIN['loginID'] = $row['ID'];
-        $_SESSION['loginUser'] = $username;
+    //Register User************************
+    if (isset($_POST["register"])){
 
 
-      //  header("location: ../index.html");
+      $sql = $conn->prepare("INSERT INTO users(NAME, PASSWORD) VALUES(?,?)");
+      $sql->bind_param("ss",$username, $password);
+
+
+      if ( $sql->execute() === TRUE) {
+          echo "Register Successful";
+
+          header("location: ../login.html");
       }
 
-    else {
-      echo "Invalid Login";
     }
+
+    //Sing In Existing User *******************
+    else{
+
+        $sql = "SELECT ID FROM users WHERE NAME = '$username' AND PASSWORD = '$password'";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+
+            echo "Login Successful";
+            $_SESSOIN['loginID'] = $row['ID'];
+            $_SESSION['loginUser'] = $username;
+
+
+            header("location: ../index.html");
+          }
+
+        else {
+          echo "Invalid Login";
+        }
+    }
+
   }
 
   $conn->close();
