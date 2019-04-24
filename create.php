c827140abaf7638d51f7943a8165e0aa716c67d8
tduvall <47044650+tduvall@users.noreply.github.com>
2019-04-24 18:43:33 -0500

    Added footer so background wraps everything

--- create.php
+++ create.php
@@ -13,29 +13,35 @@
         <ul>
             <li>Make sure all fields have placeholder text as hints</li>
         </ul>
-        <form action = "php/insertFund.php" method = "post">
-            <table>
-                <tr>
-                    <td><label for="name">*Name: </label></td>
-                    <td><input type="text" name="name"></td>
-                </tr>
-                <tr>
-                    <td><label for="image">Image: </label></td>
-                    <td><input type="file" name="image"></td>
-                </tr>
-                <tr>
-                    <td><label for="desc">Description: </label></td>
-                    <td><textarea rows="4" name="desc"></textarea></td>
-                </tr>
-                <tr>
-                    <td><label for="goal">*Goal: </label></td>
-                    <td><input type="text" name="goal"></td>
-                </tr>
-                <tr>
-                    <td><button type="submit">Submit</button></td>
-                </tr>
-            </table>
-        </form>
+        <section>
+            <form action = "php/insertFund.php" method = "post">
+                <table>
+                    <tr>
+                        <td><label for="name">*Name: </label></td>
+                        <td><input type="text" name="name"></td>
+                    </tr>
+                    <tr>
+                        <td><label for="image">Image: </label></td>
+                        <td><input type="file" name="image"></td>
+                    </tr>
+                    <tr>
+                        <td><label for="desc">Description: </label></td>
+                        <td><textarea rows="4" name="desc"></textarea></td>
+                    </tr>
+                    <tr>
+                        <td><label for="goal">*Goal: </label></td>
+                        <td><input type="text" name="goal"></td>
+                    </tr>
+                    <tr>
+                        <td><button type="submit">Submit</button></td>
+                    </tr>
+                </table>
+            </form>
+        </section>
+        <section id="footer">
+        <hr>
+        <p>We need to add some text or just a 'hr' below the table for formatting to work</p>
+        </section>
     </section>
 </body>
 </html>
--- index.php
+++ index.php
@@ -12,33 +12,34 @@
 
 </head>
 <body>
-    <section class="main">
-        <h1>Fundraiser Feed</h1>
-        <br>
-        <p>TODO:</p>
-        <ul>
-            <li> Resize table images to all be same height (Should be under the same class) </li>
-            <li> Have one of the columns be a link to fund page (Maybe save showign the description for fund page)</li>
-            <li> Fund page will have giant progress bar and ability to Donate to fund <li>
-            <li>fund.html will grab information from the previous page and display that poll's info (Taylor) </li>
-            <li>Save users image file into our images folder </li>
+  <section class="main">
+    <h1>Fundraiser Feed</h1>
+    <br>
+    <p>TODO:</p>
+    <ul>
+        <li> Resize table images to all be same height (Should be under the same class) </li>
+        <li> Have one of the columns be a link to fund page (Maybe save showign the description for fund page)</li>
+        <li> Fund page will have giant progress bar and ability to Donate to fund <li>
+        <li>fund.html will grab information from the previous page and display that poll's info (Taylor) </li>
+        <li>Save users image file into our images folder </li>
 
-        </ul>
+    </ul>
 
-        <section id = "fundContainer">
-          <table id ="fundTable">
-            <tr>
-              <th> </th>
-              <th> Name </th>
-              <th> Description </th>
-              <th> Raised </th>
-              <th> Goal </th>
-            </tr>
-          </table>
-        </section>
-
+    <section id = "fundContainer">
+      <table id ="fundTable">
+        <tr>
+          <th> </th>
+          <th> Name </th>
+          <th> Description </th>
+          <th> Raised </th>
+          <th> Goal </th>
+        </tr>
+      </table>
     </section>
-
-
+    <section id="footer">
+      <hr>
+      <p>We need to add some text or just a 'hr' below the table for formatting to work</p>
+    </section>
+  </section>
 </body>
 </html>
