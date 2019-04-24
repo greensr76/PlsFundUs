5ad80ecc0bbfea1df3559401b5c162ab6b064a36
tduvall <47044650+tduvall@users.noreply.github.com>
2019-04-24 10:21:17 -0500

    Update insertFund.php

--- php/insertFund.php
+++ php/insertFund.php
@@ -1,42 +1,32 @@
 <?php
 include('session.php');
 
-//echo $_SESSION("loginID");
-
 $name = "";
 $image = "";
 $desc = "";
 $goal = 0;
 $raised = 0;
 
-//Needs to Session User ID
-$userID = 1;
+//Only Required Fields
+if(isset($_POST["name"]) && isset($_POST["goal"])){
+  $name = $_POST["name"];
+  $goal = $_POST["goal"];
 
-  //Only Required Fields
-  if(isset($_POST["name"]) && isset($_POST["goal"])){
-    $name = $_POST["name"];
-    $goal = $_POST["goal"];
-
-    //Optional Fields
-    if(isset($_POST["image"])){
-      $image = $_POST["image"];
-    }
-    if(isset($_POST["desc"])){
-      $desc = $_POST["desc"];
-    }
-
-    $sql = $db->prepare("INSERT INTO funds(NAME, DESCRIPTION, GOAL, RAISED, IMAGE, USERID)
-    VALUES(?,?,?,0,?,?)");
-    $sql->bind_param("ssisi",$name, $desc, $goal, $image, $userID);
-
-    if ( $sql->execute() === TRUE) {
-      echo "Fund Created Successfully";
-
-    //  header("location: ../login.php");
-    }
-
-
+  //Optional Fields
+  if(isset($_POST["image"]) && $_POST["image"] != null) {
+    $image = $_POST["image"];
   }
+  if(isset($_POST["desc"])) {
+    $desc = $_POST["desc"];
+  }
+  $loginID = $_SESSION["loginID"];
 
+  $data = array($name, $desc, $goal, $raised, $image, $loginID);
+  $sql = $db->prepare("INSERT INTO funds(NAME, DESCRIPTION, GOAL, RAISED, IMAGE, USERID)
+  VALUES(?,?,?,?,?,?)");
+
+  $sql->execute(array($name, $desc, $goal, $raised, $image, $loginID));
+  header("location: ../index.php");
+}
 
 ?>
